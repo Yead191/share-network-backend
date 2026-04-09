14b2d56e1396877dcdf60dec7bbfd602a2b82f93
azizulhoq953 <azizulhoq2041@gmail.com>
2026-04-09 15:04:34 +0600

    nodemailer

--- src/helpers/emailHelper.ts
+++ src/helpers/emailHelper.ts
@@ -6,7 +6,7 @@ import { ISendEmail } from '../types/email';
 const transporter = nodemailer.createTransport({
   host: process.env.EMAIL_HOST,
   port: parseInt(process.env.EMAIL_PORT || '587'),
-  secure: false, 
+  secure: true, 
   auth: {
     user: process.env.EMAIL_USER,
     pass: process.env.EMAIL_PASS
