49920ef2400e4d02c43ce4da734e25a8fcf2d078
abdullahalnomandev <abdullahalnoman1512@gmail.com>
2026-04-20 12:53:27 +0600

    updte

--- src/helpers/emailHelper.ts
+++ src/helpers/emailHelper.ts
@@ -1,35 +1,82 @@
+// import nodemailer from 'nodemailer';
+// import config from '../config';
+// import { errorLogger, logger } from '../shared/logger';
+// import { ISendEmail } from '../types/email';
+
+
+// const transporter = nodemailer.createTransport({
+//   host: process.env.EMAIL_HOST,
+//   port: parseInt(process.env.EMAIL_PORT || '587'),
+//   secure: process.env.EMAIL_PORT === '465',
+//   auth: {
+//     user: process.env.EMAIL_USER,
+//     pass: process.env.EMAIL_PASS
+//   },
+//   tls: {
+//     rejectUnauthorized: false 
+//   },
+//   connectionTimeout: 20000, 
+//   greetingTimeout: 20000,
+//   socketTimeout: 20000,
+// });
+
+// export const sendEmail = async (mailOptions: any) => {
+//   try {
+//     const info = await transporter.sendMail(mailOptions);
+//     console.log('✅ Email sent successfully:', info.messageId);
+//     return info;
+//   } catch (error) {
+//     console.error('❌ Email sending failed:', error);
+//     throw error;
+//   }
+// };
+
+// export const emailHelper = {
+//     sendEmail
+// };
+
+
+// // EMAIL_FROM=abdullaalnuman129@gmail.com
+// // EMAIL_USER=abdullaalnuman129@gmail.com
+// // EMAIL_PASS=nbezwqzcdjvfndyo
+// // EMAIL_PORT=587
+// // EMAIL_HOST=smtp.gmail.com
+
+
 import nodemailer from 'nodemailer';
 import config from '../config';
 import { errorLogger, logger } from '../shared/logger';
 import { ISendEmail } from '../types/email';
 
 const transporter = nodemailer.createTransport({
-  host: process.env.EMAIL_HOST,
-  port: parseInt(process.env.EMAIL_PORT || '587'),
-  secure: process.env.EMAIL_PORT === '465',
+  host: config.email.host,
+  port: Number(config.email.port),
+  secure: false,
+  requireTLS: true,
   auth: {
-    user: process.env.EMAIL_USER,
-    pass: process.env.EMAIL_PASS
+    user: config.email.user,
+    pass: config.email.pass,
   },
   tls: {
-    rejectUnauthorized: false 
+    rejectUnauthorized: false,
   },
-  connectionTimeout: 20000, 
-  greetingTimeout: 20000,
-  socketTimeout: 20000,
 });
 
-export const sendEmail = async (mailOptions: any) => {
+const sendEmail = async (values: ISendEmail) => {
   try {
-    const info = await transporter.sendMail(mailOptions);
-    console.log('✅ Email sent successfully:', info.messageId);
-    return info;
+    const info = await transporter.sendMail({
+      from: `"Share Network App" <${config.email.from}>`,
+      to: values.to,
+      subject: values.subject,
+      html: values.html,
+    });
+
+    logger.info('Mail send successfully', info.accepted);
   } catch (error) {
-    console.error('❌ Email sending failed:', error);
-    throw error;
+    errorLogger.error('Email', error);
   }
 };
 
 export const emailHelper = {
-    sendEmail
+  sendEmail,
 };
